8c130bd8bc4ae4ac625aafca3c11f637da80c01a
jkousemaker <70757540+jkousemaker@users.noreply.github.com>
2024-07-02 01:19:35 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["three"],
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "plus.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "tympanus.net",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
