be33660e2d8114532b69b1f019edb42b158cd117
jkousemaker <70757540+jkousemaker@users.noreply.github.com>
2024-07-02 01:12:21 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
-/** @type {import('next').NextConfig} */
-const path = require("path");
-
 const nextConfig = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
   transpilePackages: ["three"],
   images: {
     remotePatterns: [
@@ -29,13 +23,6 @@ const nextConfig = {
       },
     ],
   },
-  rules: [
-    {
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      exclude: /node_modules/,
-      use: ["raw-loader", "glslify-loader"],
-    },
-  ],
 };
 
 module.exports = nextConfig;
