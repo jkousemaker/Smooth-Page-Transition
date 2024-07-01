c5a7002c7aee9eeb8ac668efc5074a83357f7356
jkousemaker <70757540+jkousemaker@users.noreply.github.com>
2024-07-02 00:52:19 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,41 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  transpilePackages: ["three"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "tympanus.net",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  rules: [
+    {
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ["raw-loader", "glslify-loader"],
+    },
+  ],
 };
 
-export default nextConfig;
+module.exports = nextConfig;
